de84b0d25a541eb821ab8815ddc75d41ca542a4e
Arnetta Rahma <arnettarahma@gmail.com>
2017-01-10 19:05:45 +0700

    Add project files.

---
+++ Arnetta R_XIR4_08_MVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Arnetta_R_XIR4_08_MVC.MvcApplication" Language="C#" %>
---
+++ Arnetta R_XIR4_08_MVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Arnetta_R_XIR4_08_MVC.Startup))]
+namespace Arnetta_R_XIR4_08_MVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
